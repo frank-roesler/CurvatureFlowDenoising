27af8b105b247aadd685ee6ce5682772d57db578
Frank Roesler <frank.roesler@posteo.de>
2024-12-24 13:51:25 +0100

    update patameterValues

--- parameterValues.py
+++ parameterValues.py
@@ -5,7 +5,7 @@ dt = 0.1  # time stepp for PDE solver
 iters = 100
 stencil = 1  # larger value leads to more smoothing (theoretically...)
 noiseLvl = 0.1
-plottingFreq = 50
+plottingFreq = 10
 plotDuringLoop = True
 savePlots = True
 imagePadding = "constant"  # must work for np.pad and scipy.ndimage.convolve
@@ -14,6 +14,6 @@ imagePadding = "constant"  # must work for np.pad and scipy.ndimage.convolve
 VGradient = 0.02
 
 # Thresholds for mean curvature modulation
-# (nothing < M1 < min/max flow < M2 < mean curvature flow)
+# (do nothing < M1 < min/max flow < M2 < smoothing)
 M1 = 0.01
 M2 = 0.7
